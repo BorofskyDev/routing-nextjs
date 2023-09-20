aae333c88be30289b35c94bccbd495ce02968d67
solovyovsdom <joelborofsky@mac.com>
2023-09-20 10:35:16 -0500

    added search functionality

--- pages/events/index.jsx
+++ pages/events/index.jsx
@@ -1,11 +1,20 @@
+import { useRouter } from "next/router"
 import { getAllEvents } from "../../dummy-data"
 import EventList from "../../components/events/event-list"
+import EventsSearch from "../../components/events/event-search"
 
 function AllEventsPage() {
+  const router = useRouter()
   const events = getAllEvents()
+
+  const findEventsHandler = (year, month) => {
+    const fullPath = `/events/${year}/${month}`
+    router.push(fullPath)
+  }
 
   return (
     <>
+    <EventsSearch onSearch={findEventsHandler} />
     <EventList items={events} />
     </>
   )
