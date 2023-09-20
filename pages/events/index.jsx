1fcf054264666a5a54cc0f298775964a9b7c798f
solovyovsdom <joelborofsky@mac.com>
2023-09-20 10:06:37 -0500

    added all events page

--- pages/events/index.jsx
+++ pages/events/index.jsx
@@ -1,6 +1,13 @@
+import { getAllEvents } from "../../dummy-data"
+import EventList from "../../components/events/event-list"
+
 function AllEventsPage() {
+  const events = getAllEvents()
+
   return (
-    <h1>All Events</h1>
+    <>
+    <EventList items={events} />
+    </>
   )
 }
 export default AllEventsPage
